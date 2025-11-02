27a37beaca0e3b00627873eeef4e2d9e7d207638
Alee Mendoza <alecolomendoza@gmail.com>
2025-11-01 21:38:56 -0300

    Update .gitignore

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.ethercode.com.ar',
-    generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.8,
-    sitemapSize: 5000,
-  };
-  
